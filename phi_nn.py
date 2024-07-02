c78abf315d8b08d153a9ae8ac8f794f5310d8ac5
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2024-07-02 18:56:26 +0200

    quitar dropout y triplet loss

--- phi_nn.py
+++ phi_nn.py
@@ -16,7 +16,7 @@ import wandb
 from sklearn.linear_model import LogisticRegression
 from scipy.spatial import distance
 from phi_functions import PhiPACC
-from phi_functions import PhiPequena as PPeq
+# from phi_functions import PhiPequena as PPeq
 from representationLearningQuantification import run_experiment as runexp
 
 
@@ -54,11 +54,11 @@ class PhiPequena(nn.Module):
     def __init__(self, X_shape):
         super(PhiPequena, self).__init__()
         self.module = torch.nn.Sequential()
-        self.module.add_module("linear_1", nn.Linear(X_shape, X_shape*2))
-        #self.module.add_module("relu_1", nn.ReLU())
-        self.module.add_module("sig_1", nn.Tanh())
-        self.module.add_module("dropout_1", nn.Dropout(0.5))
-        self.module.add_module("linear_2", nn.Linear(X_shape*2, X_shape//2))
+        self.module.add_module("linear_1", nn.Linear(X_shape, X_shape))
+        self.module.add_module("relu_1", nn.ReLU())
+        # self.module.add_module("sig_1", nn.Tanh())
+        # self.module.add_module("dropout_1", nn.Dropout(0.1))
+        self.module.add_module("linear_2", nn.Linear(X_shape, X_shape//2))
         #self.module.add_module("relu_2", nn.ReLU())
         """self.module.add_module("linear_3", nn.Linear(X_shape//2, 32))
         self.module.add_module("relu_3", nn.ReLU())
@@ -73,11 +73,11 @@ class PhiPequena_decoder(nn.Module):
     def __init__(self, X_shape):
         super(PhiPequena_decoder, self).__init__()
         self.module = torch.nn.Sequential()
-        self.module.add_module("linear_1_dec", nn.Linear(X_shape//2, X_shape*2))
-        #self.module.add_module("relu_1_dec", nn.ReLU())
-        self.module.add_module("sig_1_dec", nn.Tanh())
-        self.module.add_module("dropout_1_dec", nn.Dropout(0.5))
-        self.module.add_module("linear_2_dec", nn.Linear(X_shape*2, X_shape))
+        self.module.add_module("linear_1_dec", nn.Linear(X_shape//2, X_shape))
+        self.module.add_module("relu_1_dec", nn.ReLU())
+        # self.module.add_module("sig_1_dec", nn.Tanh())
+        # self.module.add_module("dropout_1_dec", nn.Dropout(0.1))
+        self.module.add_module("linear_2_dec", nn.Linear(X_shape, X_shape))
         """self.module.add_module("relu_2_dec", nn.ReLU())
         self.module.add_module("dropout_2_dec", nn.Dropout(0.5))
         self.module.add_module("linear_3_dec", nn.Linear(64, 128))
@@ -94,7 +94,7 @@ class PhiGrande(nn.Module):
     def __init__(self, phi_pequena):
         super(PhiGrande, self).__init__()
         self.phi_pequena = phi_pequena
-    
+
     def forward(self, X):
         X = self.phi_pequena(X)
         mean = torch.mean(X, dim=0)
@@ -149,7 +149,7 @@ wandb_execution_name = "phi_nn_encoder_decoder_new"
 
 if __name__ == '__main__':
     qp.environ['SAMPLE_SIZE'] = 250
-    datasets = ['academic-success'] #,'academic-success','digits','letter'] #, 'T1A'] #'dry-bean',
+    datasets = ['digits'] #,'academic-success','digits','letter'] #, 'T1A'] #'dry-bean',
 
     table_ae = Table(name='mae')
     table_rae = Table(name='mrae')
@@ -166,9 +166,14 @@ if __name__ == '__main__':
             )
 
         if dataset_name == "T1A":
-            train, test = qp.datasets.fetch_lequa2022(dataset_name).train_test
+            train, _, test_gen = qp.datasets.fetch_lequa2022(dataset_name)
         else: 
-            train, test = qp.datasets.fetch_UCIMulticlassDataset(dataset_name).train_test
+            # train, test = qp.datasets.fetch_UCIMulticlassDataset(dataset_name).train_test
+            data = qp.datasets.fetch_UCIMulticlassDataset(dataset_name)
+            qp.data.preprocessing.standardize(data, inplace=True)
+            train, test = data.train_test
+
+        print(f'{len(train)=} {len(test)=}')
         
         train, val = train.split_stratified(train_prop=0.6)
         num_classes = len(train.classes_)
@@ -181,9 +186,9 @@ if __name__ == '__main__':
             X = []
             for i in data.classes_:
                 X_i = torch.tensor(data.X[data.y==i], dtype=torch.float32)
-                phi_peqs = phi_pequena(X_i)
-                phi_gr = torch.mean(phi_peqs, dim=0)
-                #phi_gr, phi_peqs = phi_grande(X_i)
+                # phi_peqs = phi_pequena(X_i)
+                # phi_gr = torch.mean(phi_peqs, dim=0)
+                phi_gr, phi_peqs = phi_grande(X_i)
                 X.append(X_i)
                 M.append(phi_gr)
                 phi_X.append(phi_peqs)
@@ -204,7 +209,7 @@ if __name__ == '__main__':
         else: 
             params = list(phi_pequena.parameters()) + list(phi_pequena_decoder.parameters())
 
-        optimizer = optim.Adam(params, lr=0.1, weight_decay=0.0001)
+        optimizer = optim.Adam(params, lr=0.00001) #, weight_decay=0.0001)
         
         early_stopping = EarlyStopping(patience=15)
         
@@ -222,7 +227,7 @@ if __name__ == '__main__':
             #X_LM, y_LM = torch.tensor(LM.X, dtype=torch.float32), torch.tensor(LM.y, dtype=torch.int)
             #batch_size = 64 
             
-            n_epochs = 100
+            n_epochs = 50
             sample_size = random.randint(100, 500)
             for epoch in range(n_epochs):
                 upp_gen = UPP(Lq, sample_size=sample_size, random_state=None, return_type="sample_prev")
@@ -232,11 +237,12 @@ if __name__ == '__main__':
                     phi_pequena_decoder.train()
                     phi_grande.train()
                     M, phi_X, X, y = gen_M(LM, phi_grande)
+                    # M, phi_X, X, y = gen_M(LM, None)
                     sam = torch.tensor(sam, dtype=torch.float32)
                     q, Xq = phi_grande(sam)
                     
                     # TRIPLET LOSS: min(|q - M@prev| - |q - M@prev_constr|)
-                    """vect_matrix = qp.functional.uniform_prevalence_sampling(n_classes = num_classes, size= 100)
+                    vect_matrix = qp.functional.uniform_prevalence_sampling(n_classes = num_classes, size= 100)
                     p_aux = prev.reshape((1, -1))
                     dist = distance.cdist(vect_matrix, p_aux, 'minkowski', p=1)
                     ind = np.argmax(dist)
@@ -246,10 +252,10 @@ if __name__ == '__main__':
                     positive_dist = F.pairwise_distance(q, M@prev, p=2)
                     negative_dist = F.pairwise_distance(q, M@prev_contr, p=2)
             
-                    loss_reconstruction = positive_dist-negative_dist
-                    loss_contr = torch.norm(M@prev_contr - q)"""
+                    loss_triplet = positive_dist - negative_dist
+                    # loss_contr = torch.norm(M@prev_contr - q)
 
-                    prev = torch.tensor(prev, dtype=torch.float32)
+                    # prev = torch.tensor(prev, dtype=torch.float32)
                     loss_quant =  torch.norm(M@prev - q)
                     
 
@@ -261,56 +267,58 @@ if __name__ == '__main__':
 
                     if use_classification_loss:      
                         loss_classif = regularization_classif(phi_X, y, linear_classif) 
-                        loss = loss_classif + loss_quant 
+                        # loss = loss_classif + loss_quant
                         #loss = (1-(weight_index/tot_index))*loss_classif+(weight_index/tot_index)*loss_rec
                     
-                    loss =  loss_decoder
+                    loss =  loss_decoder + loss_triplet + 0*loss_quant
                     #loss = loss_reconstruction
                     
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
 
-                print(f'Age {age+1}/{n_ages} : Epoch {epoch+1}/{n_epochs}, Loss: {loss.item()}')
+                print(f'Age {age+1}/{n_ages} : Epoch {epoch+1}/{n_epochs}, Loss: {loss.item():.5f}\t{loss_triplet=:.5f}\t{loss_decoder=:.5f}\t{loss_quant=:.5f}')
+
 
             LM_val, Lq_val = val.split_stratified(train_prop=0.5)
             rep = RepresentationLearningQuantification(phi_grande.eval(), "fro")
             rep.fit(LM_val)
             report = qp.evaluation.evaluation_report(rep, UPP(Lq_val, repeats=200), error_metrics=["mrae", "mae"])
-            loss_val = np.mean(report["mrae"].values)
-            print(f'VAL MRAE={loss_val:.6f}')
+            loss_val_rae = np.mean(report["mrae"].values)
+            loss_val_ae = np.mean(report["mae"].values)
+            print(f'VAL MRAE={loss_val_rae:.6f}\tMAE={loss_val_ae:.6f}')
 
             if show_lr: 
                 decision_func = logr.decision_function(val.X)
                 ce_loss = nn.CrossEntropyLoss()
                 ce_loss = ce_loss(torch.tensor(decision_func), torch.tensor(val.y))
 
-            if use_wandb:
+            # if use_wandb:
                 #wandb.log({"train_loss": loss.item(), "val_loss": loss_val, "loss_classif": loss_classif, "loss_reconstruction": loss_quant, "LR_loss": ce_loss}, step=age)
                 ## PARA TRIPLET LOSS:
                 #wandb.log({"train_loss": loss.item(), "val_loss": loss_val, "loss_contr": loss_contr, "loss_qunat": loss_quant}, step=age)
                 ## PARA LOSS ENCODER-DECODER:
-                wandb.log({"train_loss": loss.item(), "val_loss": loss_val, "loss_qunat": loss_quant, "loss_decoder": loss_decoder}, step=age)
+                # wandb.log({"train_loss": loss.item(), "val_loss": loss_val, "loss_qunat": loss_quant, "loss_decoder": loss_decoder}, step=age)
 
-            early_stopping(loss_val)
-            if early_stopping.early_stop:
-                print("Early stopping")
-                break
+            # early_stopping(loss_val)
+            # if early_stopping.early_stop:
+            #     print("Early stopping")
+            #     break
         
         
-        LM_test, Lq_test = test.split_stratified(train_prop=0.5)
-        phi = phi_grande.eval()
-        path = f'results/{dataset_name}/phiNN__'+'.pkl'
-        result = pickled_resource(path, run_experiment, LM_test, Lq_test, phi)
-        aes = result["mae"].values
-        raes = result["mrae"].values
+        # LM_test, Lq_test = test.split_stratified(train_prop=0.5)
+        # phi = phi_grande.eval()
+        # path = f'results/{dataset_name}/phiNN__'+'.pkl'
+        # result = pickled_resource(path, run_experiment, LM_test, Lq_test, phi)
+        # aes = result["mae"].values
+        # raes = result["mrae"].values
 
-        print(f'MAE={np.mean(aes):.6f}')
-        print(f'MRAE={np.mean(raes):.6f}')
-        print()
-        table_ae.add(benchmark=dataset_name, method='phiNN', v=aes)
-        table_rae.add(benchmark=dataset_name, method='phiNN', v=raes)
-        if use_wandb:
-            wandb.finish()
+        # print(f'MAE={np.mean(aes):.6f}')
+        # print(f'MRAE={np.mean(raes):.6f}')
+        # print()
+        # table_ae.add(benchmark=dataset_name, method='phiNN', v=aes)
+        # table_rae.add(benchmark=dataset_name, method='phiNN', v=raes)
+        # if use_wandb:
+        #     wandb.finish()
     
     Table.LatexPDF('./latex/tablesPhiNN.pdf', [table_ae, table_rae], dedicated_pages=False)
